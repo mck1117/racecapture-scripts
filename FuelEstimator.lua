5f37c2de16d0d64c529a4b3d00c377ee56f5c46d
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-01 23:49:17 -0800

    script at least doesn't die immediately

--- FuelEstimator.lua
+++ FuelEstimator.lua
@@ -4,18 +4,32 @@ local fuelCapacity = 54
 local lastLapNumber = 1
 local lastFuelConsumed = 0
 
-local chLapFuel = addChannel("LastLapFuel", 1, 2, 0, 2, "kg")
+local chFuelUsed = addChannel("FuelUsed", 1, 2, 0, 100, "L")
+local chLapFuel = addChannel("LastLapFuel", 1, 2, 0, 2, "L")
 local chLapsRemaining = addChannel("Remain", 1, 0, 0, 50)
 
 setChannel(chLapFuel, 0)
 setChannel(chLapsRemaining, 0)
 
+local lastFuelRolling = 0
+local fuelUseRollover = 0
+
 function onTick()
     local currentLapNumber = getLapCount()
 
+    local currentFuelRolling = getChannel('FuelUseRoll')
+    if currentFuelRolling < lastFuelRolling then
+        -- Either the ECU rebooted or used >2 liters of fuel
+        -- Log the old rolling qty as used
+        fuelUseRollover = fuelUseRollover + lastFuelRolling
+    end
+    lastFuelRolling = currentFuelRolling
+
+    local fuelConsumed = fuelUseRollover + currentFuelRolling
+    setChannel(chFuelUsed, fuelConsumed)
+
     if currentLapNumber ~= lastLapNumber then
         -- We've just crossed start/finish line, update stats
-        local fuelConsumed = getChannel('FuelUsed')
         local lapFuel = fuelConsumed - lastFuelConsumed
         lastFuelConsumed = fuelConsumed
 
@@ -23,10 +37,10 @@ function onTick()
 
         -- Estimate how many laps left to consume 54kg
         local fuelRemaining = fuelCapacity - fuelConsumed
-  local lapsRemain = fuelRemaining / lapFuel
+        local lapsRemain = fuelRemaining / lapFuel
 
-  -- grrr sometimes math goes haywire
-  if lapsRemain > 1000 then lapsRemain = 0 end
+        -- grrr sometimes math goes haywire
+        if lapsRemain > 1000 then lapsRemain = 0 end
 
         setChannel(chLapsRemaining, lapsRemain)
 
