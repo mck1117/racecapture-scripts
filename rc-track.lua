9411f0530eac85ad6b68a204b9c836421a3c12d5
Matthew Kennedy <matthewkennedy@outlook.com>
2024-04-02 21:55:36 -0700

    combine frames

--- rc-track.lua
+++ rc-track.lua
@@ -23,18 +23,16 @@ function onTick()
 
 	txCAN(0, 100, 0, {lat1, lat2, lat3, lat4, lon1, lon2, lon3, lon4}) 
 
-	local speed = getGpsSpeed()
-	txCAN(0, 101, 0, {speed})
-
 	local gx = math.floor(50*getImu(0))
 	local gy = math.floor(50*getImu(1))
 	local gz = math.floor(50*getImu(2))
 	local rz = math.floor(50*getImu(3))
+	local speed = getGpsSpeed()
 
 	if (gx < 0) then gx=256+gx end
 	if (gy < 0) then gy=256+gy end
 	if (gz < 0) then gz=256+gz end
 	if (rz < 0) then rz=256+rz end
 
-	txCAN(0, 102, 0, {gx, gy, gz, rz})
+	txCAN(0, 101, 0, {gx, gy, gz, rz, speed})
 end
